0cbbe90e8f7cf6d31de28a2aa91c8d94df07cc6c
Rowan Molony <rowan.molony@codema.ie>
2020-08-12 11:36:42 +0100

    Revert to CI-compatible poetry.lock
    Accidentally deleted pytest-cov & pytest-xdist
    dependencies upon rebasing branch on master

--- src/drem/extract_transform_load.py
+++ src/drem/extract_transform_load.py
@@ -15,12 +15,12 @@ prefect.config.flows.checkpointing = True
 
 with Flow("Extract, Transform & Load DREM Data") as flow:
 
-    sa_geometries_raw = drem.extract_sa_geometries(EXTERNAL_DIR / "sa_geometries")
+    sa_geometries_raw = drem.extract_sa_geometries(EXTERNAL_DIR)
     sa_geometries_clean = drem.transform_sa_geometries(sa_geometries_raw)
     drem.load_sa_geometries(sa_geometries_clean)
 
-    sa_statistics_raw = drem.extract_sa_statistics(EXTERNAL_DIR / "sa_statistics")
-    sa_statistics_glossary = drem.extract_sa_glossary(EXTERNAL_DIR / "sa_glossary")
+    sa_statistics_raw = drem.extract_sa_statistics(EXTERNAL_DIR)
+    sa_statistics_glossary = drem.extract_sa_glossary(EXTERNAL_DIR)
     sa_statistics = drem.transform_sa_statistics(
         sa_statistics_raw, sa_statistics_glossary, sa_geometries_clean,
     )
