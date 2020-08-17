f2119f8f91b1881b24d643292fe89c723629936c
Rowan Molony <rowan.molony@codema.ie>
2020-08-17 11:32:17 +0100

    Extract ber, sa_geom, sa_stats to parquet (#23)
    * Extract ber, sa_geom, sa_stats to parquet
    
    Easy API to read data frome externals as always
    using read_parquet (works for geopandas and pandas!)
    
    * Remove unused variable in tests

--- src/drem/filepaths.py
+++ src/drem/filepaths.py
@@ -4,10 +4,13 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parents[2]
 SRC_DIR = Path(__file__).parent
 DATA_DIR = BASE_DIR / "data"
-TEST_DATA_DIR = BASE_DIR / "tests" / "data"
+TEST_DIR = BASE_DIR / "tests"
+TEST_DATA_DIR = TEST_DIR / "data"
 
 RAW_DIR = DATA_DIR / "raw"
 PROCESSED_DIR = DATA_DIR / "processed"
 INTERIM_DIR = DATA_DIR / "interim"
 EXTERNAL_DIR = DATA_DIR / "external"
 REQUESTS_DIR = DATA_DIR / "requests"
+
+TEST_DATA_EXTERNAL = TEST_DIR / "extract" / "data"
---
+++ tests/extract/test_ber.py
@@ -0,0 +1,35 @@
+# flake8: noqa
+
+from pathlib import Path
+from shutil import copyfile
+
+from _pytest.monkeypatch import MonkeyPatch
+
+import drem
+
+from drem.filepaths import TEST_DATA_EXTERNAL
+
+
+BER_ZIPPED = TEST_DATA_EXTERNAL / "BERPublicsearch.zip"
+BER_EOUT = TEST_DATA_EXTERNAL / "BERPublicsearch.csv"
+
+
+def mock_download_ber(*args, **kwargs) -> None:
+
+    return None
+
+
+def test_extract_ber(monkeypatch: MonkeyPatch, tmp_path: Path) -> None:
+    """Extracted BER DataFrame parquet exists.
+
+    Args:
+        monkeypatch (MonkeyPatch): a pytest plugin to mock out objects
+        tmpdir (py.path.local): a pytest plugin to create temporary directories
+    """
+    copyfile(BER_ZIPPED, tmp_path / "BERPublicsearch.zip")
+    monkeypatch.setattr(drem.extract.ber, "_download_ber", mock_download_ber)
+
+    drem.extract_ber.run("fake-email@fake-company.ie", tmp_path)
+    expected_file_output = tmp_path / "BERPublicsearch.parquet"
+
+    assert expected_file_output.exists()
