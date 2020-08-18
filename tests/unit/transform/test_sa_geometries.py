25952373d03cbcbd1f428a300e94a7eb140177bb
Rowan Molony <rowan.molony@codema.ie>
2020-08-18 16:06:18 +0100

    Link Small Areas to Postcodes (#28)
    * Test only extracted df contains only dublin las
    
    * Link small areas to geometries
    
    * Fix broken test
    
    Don't know how this got through CI last time...
    
    * Fix test - wrong ref data filename

--- tests/unit/transform/test_sa_geometries.py
+++ tests/unit/transform/test_sa_geometries.py
@@ -1,16 +1,51 @@
 from pathlib import Path
 
 import geopandas as gpd
+import pandas as pd
 
 from geopandas.testing import assert_geodataframe_equal
+from pandas.testing import assert_frame_equal
 
 import drem
 
 from drem.filepaths import UTEST_DATA_TRANSFORM
+from drem.transform.sa_geometries import _extract_dublin_local_authorities
 
 
 SAS_IN: Path = UTEST_DATA_TRANSFORM / "sa_geometries_raw.parquet"
 SAS_EOUT: Path = UTEST_DATA_TRANSFORM / "dublin_sa_geometries_clean.parquet"
+
+
+def test_extract_dublin_local_authorities() -> None:
+    """Extracted DataFrame contains only Dublin local authorities."""
+    geometries = pd.DataFrame(
+        {
+            "COUNTYNAME": [
+                "Kildare",
+                "Dun Laoghaire-Rathdown",
+                "Fingal",
+                "South Dublin",
+                "Dublin City",
+            ],
+        },
+    )
+
+    expected_output = pd.DataFrame(
+        {
+            "COUNTYNAME": [
+                "Dun Laoghaire-Rathdown",
+                "Fingal",
+                "South Dublin",
+                "Dublin City",
+            ],
+        },
+    )
+
+    output = _extract_dublin_local_authorities(geometries).reset_index(drop=True)
+
+    assert_frame_equal(
+        output, expected_output,
+    )
 
 
 def test_transform_sa_geometries() -> None:
--- tests/unit/transform/test_sa_statistics.py
+++ tests/unit/transform/test_sa_statistics.py
@@ -4,23 +4,23 @@ import geopandas as gpd
 import pandas as pd
 
 from geopandas.testing import assert_geodataframe_equal
+from shapely.geometry import Polygon
 from tdda.referencetest.referencetest import ReferenceTest
 
 from drem import transform_sa_statistics
 from drem.filepaths import UTEST_DATA_TRANSFORM
-from drem.transform.sa_statistics import _clean_year_built_columns
-from drem.transform.sa_statistics import _extract_dublin_small_areas
-from drem.transform.sa_statistics import _extract_year_built
-from drem.transform.sa_statistics import _melt_year_built_columns
+from drem.transform import sa_statistics
 
 
 STATS_IN: Path = UTEST_DATA_TRANSFORM / "sa_statistics_raw.csv"
 GLOSSARY: Path = UTEST_DATA_TRANSFORM / "sa_glossary.xlsx"
-GEOMETRIES: Path = UTEST_DATA_TRANSFORM / "dublin_sa_geometries_clean.parquet"
+SA_GEOMETRIES: Path = UTEST_DATA_TRANSFORM / "dublin_sa_geometries_clean.parquet"
+POSTCODE_GEOMETRIES: Path = UTEST_DATA_TRANSFORM / "dublin_postcodes_clean.parquet"
 
 EXTRACT_EOUT: Path = UTEST_DATA_TRANSFORM / "dublin_sa_extract_year_built.csv"
 MELT_EOUT: Path = UTEST_DATA_TRANSFORM / "dublin_sa_melt_year_built.csv"
 CLEANED_EOUT: Path = UTEST_DATA_TRANSFORM / "dublin_sa_clean_year_built.csv"
+
 STATS_EOUT: Path = UTEST_DATA_TRANSFORM / "dublin_sa_statistics_clean.parquet"
 
 
@@ -33,7 +33,7 @@ def test_extract_year_built(ref: ReferenceTest) -> None:
     statistics = pd.read_csv(STATS_IN)
     glossary = pd.read_excel(GLOSSARY, engine="openpyxl")
 
-    output = _extract_year_built(statistics, glossary)
+    output = sa_statistics._extract_year_built(statistics, glossary)
 
     ref.assertDataFrameCorrect(output, EXTRACT_EOUT)
 
@@ -46,7 +46,7 @@ def test_melt_year_built_columns(ref: ReferenceTest) -> None:
     """
     statistics = pd.read_csv(EXTRACT_EOUT)
 
-    output = _melt_year_built_columns(statistics)
+    output = sa_statistics._melt_year_built_columns(statistics)
 
     ref.assertDataFrameCorrect(output, MELT_EOUT)
 
@@ -59,7 +59,7 @@ def test_clean_year_built_columns(ref: ReferenceTest) -> None:
     """
     statistics = pd.read_csv(MELT_EOUT)
 
-    output = _clean_year_built_columns(statistics)
+    output = sa_statistics._clean_year_built_columns(statistics)
 
     ref.assertDataFrameCorrect(output, CLEANED_EOUT)
 
@@ -67,11 +67,49 @@ def test_clean_year_built_columns(ref: ReferenceTest) -> None:
 def test_extract_dublin_small_areas() -> None:
     """Extracted Dublin small areas match (manually generated) file."""
     statistics = pd.read_csv(CLEANED_EOUT)
-    geometries = gpd.read_parquet(GEOMETRIES)
+    geometries = gpd.read_parquet(SA_GEOMETRIES)
 
-    output = _extract_dublin_small_areas(statistics, geometries)
+    output = sa_statistics._extract_dublin_small_areas(statistics, geometries)
 
     expected_output = gpd.read_parquet(STATS_EOUT)
+    assert_geodataframe_equal(output, expected_output)
+
+
+def test_link_small_areas_to_postcodes() -> None:
+    """Small Areas that are 'mostly' in Postcode are linked to Postcode."""
+    small_areas = gpd.GeoDataFrame(
+        {
+            "small_area": [1, 2],
+            "geometry": [
+                Polygon([(1, 0), (1, 1), (3, 1)]),
+                Polygon([(1, 0), (1, 1), (0, 1)]),
+            ],
+        },
+    )
+
+    postcodes = gpd.GeoDataFrame(
+        {
+            "postcodes": ["Co. Dublin", "Dublin 1"],
+            "geometry": [
+                Polygon([(0, 0), (3, 0), (0, 3)]),  # only overlaps with small_area=1
+                Polygon([(3, 3), (0, 3), (3, 0)]),  # mostly overlaps with small_area==1
+            ],
+        },
+    )
+
+    expected_output = gpd.GeoDataFrame(
+        {
+            "small_area": [1, 2],
+            "geometry": [
+                Polygon([(1, 0), (1, 1), (3, 1)]),
+                Polygon([(1, 0), (1, 1), (0, 1)]),
+            ],
+            "postcodes": ["Co. Dublin", "Co. Dublin"],
+        },
+    )
+
+    output = sa_statistics._link_small_areas_to_postcodes(small_areas, postcodes)
+
     assert_geodataframe_equal(output, expected_output)
 
 
@@ -79,8 +117,19 @@ def test_transform_sa_statistics() -> None:
     """Transformation pipeline of statistics matches reference file."""
     statistics = pd.read_csv(STATS_IN)
     glossary = pd.read_excel(GLOSSARY, engine="openpyxl")
-    geometries = gpd.read_parquet(GEOMETRIES)
+    geometries = gpd.read_parquet(SA_GEOMETRIES)
 
+    """DOES:
+    - Extracts year built columns via glossary
+    - Melts year built columns into rows for each year built type
+    - Clean year built column:
+        $ remove irrelevant substrings
+        $ drop irrelevant columns
+        $ rename columns
+        $ set dtypes
+    - Extract Dublin Small Areas
+    - Link Small Areas to Postcodes
+    """
     output = transform_sa_statistics.run(statistics, glossary, geometries)
 
     expected_output = gpd.read_parquet(STATS_EOUT)
