902a4ef4d23c0b625f684b8089c38a3b24b36734
Rowan Molony <rowan.molony@codema.ie>
2020-08-18 09:15:45 +0100

    Test only extracted df contains only dublin las

--- tests/unit/transform/test_sa_geometries.py
+++ tests/unit/transform/test_sa_geometries.py
@@ -6,11 +6,42 @@ from geopandas.testing import assert_geodataframe_equal
 
 import drem
 
+from drem.extract.sa_geometries import _extract_dublin_local_authorities
 from drem.filepaths import UTEST_DATA_TRANSFORM
 
 
 SAS_IN: Path = UTEST_DATA_TRANSFORM / "sa_geometries_raw.parquet"
-SAS_EOUT: Path = UTEST_DATA_TRANSFORM / "dublin_sa_geometries_clean.parquet"
+SAS_EOUT: Path = UTEST_DATA_TRANSFORM / "sa_geometries_clean.parquet"
+
+
+def test_extract_dublin_local_authorities() -> None:
+    """Extracted DataFrame contains only Dublin local authorities."""
+    geometries = gpd.GeoDataFrame(
+        {
+            "COUNTYNAME": [
+                "Kildare",
+                "Dun Laoghaire-Rathdown",
+                "Fingal",
+                "South Dublin",
+                "Dublin City",
+            ],
+        },
+    )
+
+    expected_output = gpd.GeoDataFrame(
+        {
+            "COUNTYNAME": [
+                "Dun Laoghaire-Rathdown",
+                "Fingal",
+                "South Dublin",
+                "Dublin City",
+            ],
+        },
+    )
+
+    output = _extract_dublin_local_authorities(geometries)
+
+    assert_geodataframe_equal(output, expected_output)
 
 
 def test_transform_sa_geometries() -> None:
