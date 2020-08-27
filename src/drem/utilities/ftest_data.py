c10e32687658ee99ac819d1429c27679c1390422
Rowan Molony <rowan.molony@codema.ie>
2020-08-27 10:13:48 +0100

    Create functional test data with functions for each
    Was using over-complicated parquet metadata method whereby
    metadata is read prior to sampling data.  This is unnecessarily
    complex when a function per data set suffices...  Easier to
    write and easier to change

---
+++ src/drem/utilities/ftest_data.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+# Create sample data for functional tests.
+
+from pathlib import Path
+from shutil import copyfile
+
+import geopandas as gpd
+import pandas as pd
+
+
+def _copy_sa_glossary(input_dirpath: Path, output_dirpath: Path) -> None:
+
+    input_filepath = input_dirpath / "sa_glossary.parquet"
+    output_filepath = output_dirpath / "sa_glossary.parquet"
+
+    if not output_filepath.exists():
+        copyfile(input_filepath, output_filepath)
+
+
+def _sample_sa_geometries(input_dirpath: Path, output_dirpath: Path) -> None:
+
+    input_filepath = input_dirpath / "sa_geometries.parquet"
+    output_filepath = output_dirpath / "sa_geometries.parquet"
+
+    if not output_filepath.exists():
+        gpd.read_parquet(input_filepath).sample(200).to_parquet(output_filepath)
+
+
+def _sample_sa_statistics(input_dirpath: Path, output_dirpath: Path) -> None:
+
+    input_filepath = input_dirpath / "sa_statistics.parquet"
+    output_filepath = output_dirpath / "sa_statistics.parquet"
+
+    if not output_filepath.exists():
+        pd.read_parquet(input_filepath).sample(200).to_parquet(output_filepath)
+
+
+def _copy_dublin_postcodes(input_dirpath: Path, output_dirpath: Path) -> None:
+
+    input_filepath = input_dirpath / "dublin_postcodes.parquet"
+    output_filepath = output_dirpath / "dublin_postcodes.parquet"
+
+    if not output_filepath.exists():
+        copyfile(input_filepath, output_filepath)
+
+
+def _sample_berpublicsearch(input_dirpath: Path, output_dirpath: Path) -> None:
+
+    input_filepath = input_dirpath / "BERPublicsearch.parquet"
+    output_filepath = output_dirpath / "BERPublicsearch.parquet"
+
+    if not output_filepath.exists():
+        pd.read_parquet(input_filepath).sample(200).to_parquet(output_filepath)
+
+
+def create_ftest_data(input_dirpath: Path, output_dirpath: Path) -> None:
+    """Create sample data for functional testing.
+
+    Args:
+        input_dirpath (Path): Path to directory containing input data
+        output_dirpath (Path): Path to directory where output data will be saved
+    """
+    _copy_sa_glossary(input_dirpath, output_dirpath)
+    _sample_sa_geometries(input_dirpath, output_dirpath)
+    _sample_sa_statistics(input_dirpath, output_dirpath)
+    _copy_dublin_postcodes(input_dirpath, output_dirpath)
+    _sample_berpublicsearch(input_dirpath, output_dirpath)
