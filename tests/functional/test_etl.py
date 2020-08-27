ea21af4280195bed830078d3bb079f5495cc7248
Rowan Molony <rowan.molony@codema.ie>
2020-08-27 12:16:08 +0100

    Mock out PrefectSecret in ftest flow run

--- tests/functional/test_etl.py
+++ tests/functional/test_etl.py
@@ -2,23 +2,44 @@
 
 from pathlib import Path
 
+import pytest
+
+from _pytest.monkeypatch import MonkeyPatch
+from prefect import Task
+from prefect.engine.state import State
 from prefect.utilities.debug import raise_on_exception
 
 from drem import etl
 from drem.filepaths import FTEST_DATA
 
 
-def mock_prefect_secret(*args, **kwargs) -> str:
-
+def mock_prefectsecret_run(name: str = None) -> str:
     return "fake-email@fake-company.ie"
 
 
-def test_etl_runs_without_errors(monkeypatch) -> None:
+@pytest.fixture
+def etl_flow_state(monkeypatch: MonkeyPatch) -> State:
+    """Run etl flow with dummy test data.
 
+    Args:
+        monkeypatch (MonkeyPatch): Pytest fixture to mock out objects s.a. PrefectSecret
+
+    Returns:
+        [State]: A Prefect State object containing flow run information
+    """
     monkeypatch.setattr(
-        etl, "PrefectSecret", mock_prefect_secret,
+        etl.PrefectSecret, "run", mock_prefectsecret_run,
     )
     with raise_on_exception():
         state = etl.flow.run(data_dir=FTEST_DATA)
 
-    assert state.is_successful()
+    return state
+
+
+def test_no_etl_tasks_fail(etl_flow_state: State) -> None:
+    """No etl tasks fail.
+
+    Args:
+        etl_flow_state (State): A Prefect State object containing flow run information
+    """
+    assert etl_flow_state.is_successful()
