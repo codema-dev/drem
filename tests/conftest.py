fbc4afa70e89cdb2ab55607acbaa37c8b189cc3c
Rowan Molony <rowan.molony@codema.ie>
2020-08-27 15:53:01 +0100

    Block http requests in pytest conftest.py (#65)
    * Block http requests in pytest conftest.py
    
    So that ftest pipeline does not accidentally try to
    download files via http requests
    
    * Test if GET request is blocked by conftest fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+from _pytest.monkeypatch import MonkeyPatch
 from tdda.referencetest import referencepytest
 
 
@@ -16,3 +17,23 @@ def pytest_collection_modifyitems(session, config, items):
 @pytest.fixture(scope="module")
 def ref(request):
     return referencepytest.ref(request)
+
+
+@pytest.fixture(autouse=True)
+def no_http_requests(monkeypatch):
+    """Prevent any test from making HTTP requests.
+
+    Source: https://blog.jerrycodes.com/no-http-requests/
+
+    Args:
+        monkeypatch (MonkeyPatch): [description]
+    """
+
+    def urlopen_mock(self, method, url, *args, **kwargs):
+        raise RuntimeError(
+            f"The test was about to {method} {self.scheme}://{self.host}{url}",
+        )
+
+    monkeypatch.setattr(
+        "urllib3.connectionpool.HTTPConnectionPool.urlopen", urlopen_mock,
+    )
---
+++ tests/unit/utilities/test_http_request_is_blocked.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import pytest
+
+from drem.extract.download import download
+
+
+def test_task_http_request_is_blocked(tmp_path: Path) -> None:
+    """No_http_requests fixture in conftest blocks http requests.
+
+    Args:
+        tmp_path (Path): A Pytest plugin to create a temporary path
+    """
+    with pytest.raises(RuntimeError):
+        download("http://www.url-to-some-file.com", tmp_path)
