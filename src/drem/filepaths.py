cfd186623200de3050552f12bd5bba1bfc3b3903
Rowan Molony <rowan.molony@codema.ie>
2020-09-07 17:41:10 +0100

    Test all data dirs are in source control (#92)
    * Test all data dirs are in source control
    
    "external" dir wasn't in source control which meant that
    new users couldn't download data files as extract tasks
    by default use the "external" folder - and can't do so if
    it doesn't exist!!!
    
    * Parametrize Test Data Dir Exists
    
    So have 1 test in place of 6

--- src/drem/filepaths.py
+++ src/drem/filepaths.py
@@ -12,7 +12,7 @@ INTERIM_DIR = DATA_DIR / "interim"
 EXTERNAL_DIR = DATA_DIR / "external"
 REQUESTS_DIR = DATA_DIR / "requests"
 ROUGHWORK_DIR = DATA_DIR / "roughwork"
-LINKS_DIR = DATA_DIR / "links"
+COMMERCIAL_BENCHMARKS_DIR = DATA_DIR / "commercial_building_benchmarks"
 
 FTEST_DATA = TEST_DIR / "functional" / "data"
 FTEST_DATA_EXTERNAL = TEST_DIR / "functional" / "data" / "external"
