96a167e31d3232451bf438fff100cf4b02495ab1
Rowan Molony <rowan.molony@codema.ie>
2020-09-10 16:28:26 +0100

    Save VO to file at end of commercial ETL (#99)
    * Save VO to file at end of commercial ETL
    
    * BUG: Forgot to pass monkeypatch to test comm etl

--- tests/functional/test_etl_commercial.py
+++ tests/functional/test_etl_commercial.py
@@ -13,13 +13,22 @@ from drem.etl import commercial
 from drem.filepaths import FTEST_DATA
 
 
+def mock_task_run(*args, **kwargs) -> None:
+    return None
+
+
 @pytest.fixture
-def etl_flow_state() -> State:
+def etl_flow_state(monkeypatch) -> State:
     """Run etl flow with dummy test data.
+
+    Args:
+        monkeypatch (MonkeyPatch): Pytest fixture to mock out objects s.a. PrefectSecret
 
     Returns:
         [State]: A Prefect State object containing flow run information
     """
+    # Mock out task load as CI doesn't need flow outputs on-disk
+    monkeypatch.setattr(commercial.drem.LoadToParquet, "run", mock_task_run)
     with raise_on_exception():
         state: State = commercial.flow.run(data_dir=FTEST_DATA)
 
