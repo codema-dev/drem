a5056be16d788be48908bd5f11f2418d00523e29
Rowan Molony <rowan.molony@codema.ie>
2020-09-15 12:18:52 +0000

    Clean CRU Smart Meter Elec Data via Dask
    Use Dask & Prefect to create a pipeline to clean
    electricity data; dask req as data is ~3GB in memory
    even after setting appropriate dtypes...
    
    Encapsulated this Prefect Flow within a Prefect Task so that the
    this cleaning flow can be run within other prefect flows and
    also enables `flow.visualize` to be called upon a `transform` task;
    not possible for other `geopandas` or `pandas` cleaning pipelines

---
+++ src/drem/transform/cru_electricity.py
@@ -0,0 +1,126 @@
+from pathlib import Path
+from typing import Any
+from typing import Iterable
+
+import dask.dataframe as dpd
+
+from prefect import Flow
+from prefect import Task
+from prefect import task
+from prefect.engine.state import State
+
+import drem
+
+
+@task
+def _read_txt_files(dirpath: Path) -> Iterable[dpd.DataFrame]:
+
+    return [
+        dpd.read_csv(
+            filepath,
+            sep=" ",
+            header=None,
+            names=["id", "timeid", "demand"],
+            dtype={"id": "int16", "timeid": "string", "demand": "float32"},
+        )
+        for filepath in dirpath.glob("*.txt")
+    ]
+
+
+@task
+def _concat_ddfs(ddfs: Iterable[dpd.DataFrame]) -> dpd.DataFrame:
+
+    return dpd.concat(ddfs)
+
+
+@task
+def _slice_timeid_column(ddf: dpd.DataFrame) -> dpd.DataFrame:
+
+    ddf["day"] = ddf["timeid"].str.slice(0, 3).astype("int16")
+    ddf["halfhourly_id"] = ddf["timeid"].str.slice(3, 5).astype("int8")
+
+    return ddf.drop(columns=["timeid"])
+
+
+@task
+def _convert_dayid_to_datetime(ddf: dpd.DataFrame) -> dpd.DataFrame:
+
+    ddf["datetime"] = dpd.to_datetime(
+        ddf["day"], origin="01/01/2009", unit="D",
+    ) + dpd.to_timedelta(ddf["halfhourly_id"] / 2, unit="h")
+
+    return ddf.drop(columns=["day", "halfhourly_id"])
+
+
+@task
+def _to_parquet(ddf: dpd.DataFrame, savepath: Path):
+
+    return ddf.to_parquet(savepath)
+
+
+class CleanCRUElecDemand(Task):
+    """Clean CRU Electricity Demands.
+
+    From:
+
+    0     1       2
+    1392  19503   0.14
+
+    To:
+
+    id      datetime                demand
+    1392    2009-07-15 01:30:00     0.14
+
+    Args:
+        Task (prefect.Task): see https://docs.prefect.io/core/concepts/tasks.html
+    """
+
+    def __init__(
+        self, dirpath: Path, savepath: Path, **kwargs: Any,
+    ):
+        """Initialise class with paths and flow executor.
+
+        Args:
+            dirpath (Path): Path to directory containing raw data files
+            savepath (Path): Path where clean data will be saved
+            **kwargs (Any): Keyword arguments that will be passed to the Task
+                constructor Task, see https://docs.prefect.io/api/latest/core/task.html
+        """
+        self.dirpath = dirpath
+        self.savepath = savepath
+
+        super().__init__(name="Clean CRU Electricity Demands", **kwargs)
+
+    def flow(self) -> Flow:
+        """Instantiate prefect task flow.
+
+        Returns:
+            Flow: see https://docs.prefect.io/core/concepts/flows.html
+        """
+        with Flow("Transform CRU Smart Meter Data") as fw:
+
+            ddfs = _read_txt_files(self.dirpath)
+            demand_raw = _concat_ddfs(ddfs)
+            demand_with_times = _slice_timeid_column(demand_raw)
+            demand_with_datetimes = _convert_dayid_to_datetime(demand_with_times)
+            _to_parquet(demand_with_datetimes, self.savepath)
+
+        return fw
+
+    def run(self) -> State:
+        """Run prefect flow.
+
+        Returns:
+            State: See https://docs.prefect.io/core/concepts/flows.html#running-a-flow
+        """
+        flow = self.flow()
+        return flow.run()
+
+
+if __name__ == "__main__":
+
+    clean_cru_elec_demand = CleanCRUElecDemand(
+        dirpath=drem.filepaths.RAW_DIR / "SM_electricity",
+        savepath=drem.filepaths.PROCESSED_DIR / "SM_electricity",
+    )
+    clean_cru_elec_demand.run()
