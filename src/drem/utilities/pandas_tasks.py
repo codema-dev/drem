68b7ba912e5f9fde74a2debcefd26042a7f4328f
Rowan Molony <rowan.molony@codema.ie>
2020-10-01 08:37:05 +0000

    Add utility task to sum pd df columns

--- src/drem/utilities/pandas_tasks.py
+++ src/drem/utilities/pandas_tasks.py
@@ -1,4 +1,5 @@
 from typing import Iterable
+from typing import Union
 
 import pandas as pd
 
@@ -22,3 +23,26 @@ def get_columns(df: pd.DataFrame, column_names: Iterable[str]) -> pd.DataFrame:
         pd.DataFrame: A new DataFrame containing only the specified columns
     """
     return df.copy().loc[:, column_names]
+
+
+@task
+@require(
+    lambda df, target: set(target).issubset(set(df.columns)),
+    "df.columns doesn't contain all names in columns!",
+)
+def get_sum_of_columns(
+    df: pd.DataFrame, target: Union[str, Iterable[str]], result: str,
+) -> pd.DataFrame:
+    """Get sum of target DataFrame columns.
+
+    Args:
+        df (pd.DataFrame): Any single-indexed Pandas DataFrame
+        target (Union[str, Iterable[str]]): Names of columns to be summed
+        result (str): Name of result column
+
+    Returns:
+        pd.DataFrame: [description]
+    """
+    df[result] = df[target].copy().sum(axis=1)
+
+    return df
--- tests/unit/utilities/test_pandas_tasks.py
+++ tests/unit/utilities/test_pandas_tasks.py
@@ -14,9 +14,9 @@ def test_get_columns_raises_error_if_passed_nonexistent_column_name() -> None:
         pdt.get_columns.run(i_am_data, ["my_name_is", "i_dont_exist"])
 
 
-def test_get_columns_raises_error_for_multiindex_df_index() -> None:
-    """Raise error if index is MultiIndex."""
+def test_sum_columns_raises_error_if_passed_nonexistent_column_name() -> None:
+    """Raise error if passed non-existent column name."""
     i_am_data = pd.DataFrame({"my_name_is": ["what"]})
 
     with pytest.raises(ViolationError):
-        pdt.get_columns.run(i_am_data, ["my_name_is", "i_dont_exist"])
+        pdt.get_sum_of_columns.run(i_am_data, ["my_name_is", "i_dont_exist"])
