0494fbb80361bd86e9e5ff3ca8261d488b97a011
Rowan Molony <rowan.molony@codema.ie>
2020-10-07 08:56:25 +0000

    Refactor unzip shp files into unzip task

--- src/drem/utilities/convert.py
+++ src/drem/utilities/convert.py
@@ -8,8 +8,6 @@ import prefect
 
 from prefect import task
 
-from drem.utilities.zip import unzip_file
-
 
 @task
 def excel_to_parquet(dirpath: Path, filename: str) -> None:
@@ -54,33 +52,17 @@ def csv_to_parquet(
 
 @task
 def shapefile_to_parquet(
-    dirpath: Path,
-    filename: str,
-    zipped: Optional[bool] = False,
-    path_to_shapefile: Optional[str] = None,
+    dirpath: Path, filename: str, path_to_shapefile: Optional[str] = None,
 ) -> None:
     """Convert ESRI Shapefile to parquet.
-
-    Note:
-        Zipped shapefiles are supported by GeoPandas. If the dataset is in a folder in
-            the ZIP file, you have to append its name:
-            `zipfile = "gadm36_AFG_shp.zip!data"`
-            If there are multiple datasets in a folder in the ZIP file, you also have
-            to specify the filename:
-            `zipfile = "gadm36_AFG_shp.zip!data/gadm36_AFG_1.shp"`
 
     Args:
         dirpath (Path): Path to input directory
         filename (str): Name of file
-        zipped (Optional[bool], optional): File extension (such as 'zip'). Defaults to
-            False.
         path_to_shapefile (Optional[str], optional): Path to shapefile from within
-            zipped folder. Defaults to None.
+            folder (if '.shp' is nested in an inner-folder). Defaults to None.
     """
     logger = prefect.context.get("logger")
-
-    if zipped:
-        unzip_file(dirpath / f"{filename}.zip")
 
     if path_to_shapefile:
         filepath_shapefile = dirpath / filename / path_to_shapefile
