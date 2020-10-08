d3e505ef59ff8ea98d4a64378c1b61ca4efce15a
Oisin Doherty <oisin.doherty@codema.ie>
2020-10-08 15:12:15 +0000

    Included surroundings esri shapefile creation

--- src/drem/utilities/geodir.py
+++ src/drem/utilities/geodir.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import geopandas as gpd
 from drem.filepaths import RAW_DIR
+from drem.filepaths import PROCESSED_DIR
 import shapely
 import pyproj
 import utm
@@ -16,50 +17,28 @@ df3_dropped = df3.drop_duplicates()
 
 buildings = gpd.read_file(RAW_DIR / 'prime2_no_z_2157.shp')
 buildings_geo = gpd.GeoDataFrame(buildings)
-buildings_final = buildings_final.to_crs(epsg=4326, inplace=True)
-buildings_final = gpd.GeoDataFrame(geometry=gpd.GeoSeries(buildings_geo['geometry']))
-buildings_flipped = buildings_final.geometry.map(lambda polygon: shapely.ops.transform(lambda x, y: (y, x), polygon))
-buildings_cea = gpd.GeoDataFrame(buildings_flipped)
-buildings_cea = buildings_cea.set_crs(epsg=4326)
+buildings_final = buildings_geo.set_crs(epsg=2157)
+buildings_final = buildings_final.to_crs(epsg=32629)
+buildings_final = gpd.GeoDataFrame(geometry=gpd.GeoSeries(buildings_final['geometry']))
+#buildings_flipped = buildings_final.geometry.map(lambda polygon: shapely.ops.transform(lambda x, y: (y, x), polygon))
+buildings_cea = gpd.GeoDataFrame(buildings_final)
 
-
-#Maybe Area too
 #Change floors to an integer, height can be a float
-df_extracted = df3_dropped[['BUILDING_ID','BUILDING_HEIGHT', 'FLOORS', 'BUILDING_USE_y', 'LATITUDE', 'LONGITUDE']]
-
+df_extracted = df3_dropped[['BUILDING_ID','BUILDING_HEIGHT', 'FLOORS', 'BUILDING_USE_y', 'ITM_EAST', 'ITM_NORTH']]
 df_renamed = df_extracted.rename(columns={'BUILDING_ID': 'Name', 'BUILDING_HEIGHT' : 'height_ag', 'FLOORS': 'floors_ag', 'BUILDING_USE_y' : 'category'})
-
 df_renamed['floors_bg'] = 0
 df_renamed['height_bg'] = 0
-
-df_ordered = df_renamed[['Name', 'height_ag', 'height_bg', 'floors_ag', 'floors_bg', 'category', 'LATITUDE', 'LONGITUDE']]
-
+df_ordered = df_renamed[['Name', 'height_ag', 'height_bg', 'floors_ag', 'floors_bg', 'category', 'ITM_EAST', 'ITM_NORTH']]
 df_final = df_ordered.drop_duplicates()
+gdf = gpd.GeoDataFrame(df_final, geometry=gpd.points_from_xy(df_final.ITM_EAST, df_final.ITM_NORTH))
 
-gdf = gpd.GeoDataFrame(df_final, geometry=gpd.points_from_xy(df_final.LATITUDE, df_final.LONGITUDE))
-
-#EPSG old one was 4326
-#Ireland epsg=32633
-#Google maps: 3857
-#Google earth: 4326
-gdf = gdf.set_crs(epsg=4326)
-
-## Extract Columns to suit CEA input
-## Geodir: BUILDING_ID, BUILDING_HEIGHT, FLOORS, NULL, NULL, 
-## CEA requires Index(['Name', 'height_ag', 'floors_ag', 'height_bg', 'floors_bg',
-## 'descriptio', 'category', 'REFERENCE', 'geometry']
-##
-## Rename Columns
-
-#gdf.to_file(driver = 'ESRI Shapefile', filename = 'GeodirSampleGeoDataFrame')
+#Setting at Irish grid and converting to UTM
+gdf = gdf.set_crs(epsg=2157) 
+gdf = gdf.to_crs(epsg=32629)
 
 points_in_poly = gpd.sjoin(buildings_cea, gdf, op='contains') 
-
-#points_in_poly = points_in_poly.to_crs("+proj=utm +zone=29 +ellps=GRS80 +datum=WGS84 +units=m +no_def")
-
 df_cea = points_in_poly[['Name','height_ag', 'height_bg', 'floors_ag', 'floors_bg', 'geometry', 'category']]
 df_cea['Name'] = 'B' + df_cea['Name'].astype(str)
-
 df_cea_height = df_cea.drop(df_cea[df_cea['height_ag']/df_cea['floors_ag'] < 1].index)
 df_cea_height = df_cea_height.reset_index()
 
@@ -68,4 +47,13 @@ df_cea_extracted = df_cea_height[['Name','height_ag', 'height_bg', 'floors_ag', 
 df_cea_extracted[['floors_ag', 'floors_bg']] = df_cea_extracted[['floors_ag', 'floors_bg']].astype(int)
 
 df_output = df_cea_extracted.drop_duplicates('Name', keep='last')
-df_output.to_file(driver = 'ESRI Shapefile', filename = 'GeodirSampleCEA')
+#Zone shapefile
+df_output.to_file(driver = 'ESRI Shapefile', filename = PROCESSED_DIR / 'ZoneCEA')
+site = points_in_poly['geometry']
+#Site shapefile
+site.to_file(driver = 'ESRI Shapefile', filename = PROCESSED_DIR / 'SiteCEA')
+#Create a surroundings shapefile with Name/height_ag/floors_ag/description/category/geometry
+surroundings = df_output[['Name','height_ag', 'floors_ag', 'geometry', 'category']]
+surroundings['description'] = 'none'
+surroundings_ordered = surroundings[['Name', 'height_ag', 'floors_ag', 'description', 'category', 'geometry']]
+surroundings_ordered.to_file(driver = 'ESRI Shapefile', filename = PROCESSED_DIR / 'SurroundingsCEA')
