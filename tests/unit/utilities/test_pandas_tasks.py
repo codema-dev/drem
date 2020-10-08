cdcb405a3cc506cd0db2c21f6bdb8838f7ceea9b
Rowan Molony <rowan.molony@codema.ie>
2020-10-08 15:10:02 +0000

    Remove check type is dataframe test
    Am no longer checking types in pandas_tasks so
    funcs work easily with geopandas objects.  In future
    will implement a generalisable contract to check types...

--- tests/unit/utilities/test_pandas_tasks.py
+++ tests/unit/utilities/test_pandas_tasks.py
@@ -17,14 +17,6 @@ def test_get_columns_raises_error_if_passed_nonexistent_column_name() -> None:
         pdt.get_columns.run(i_am_data, ["my_name_is", "i_dont_exist"])
 
 
-def test_get_columns_raises_error_if_passed_non_dataframe() -> None:
-    """Raise error if passed non-existent column name."""
-    i_am_not_a_dataframe = 12
-
-    with pytest.raises(ViolationError):
-        pdt.get_columns.run(i_am_not_a_dataframe, ["my_name_is", "i_dont_exist"])
-
-
 def test_sum_columns_raises_error_if_passed_nonexistent_column_name() -> None:
     """Raise error if passed non-existent column name."""
     i_am_data = pd.DataFrame({"my_name_is": ["what"]})
