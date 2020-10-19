ae994a3cb3f1b13291276cf2b6ee817a9b787e12
Oisin Doherty <oisin.doherty@codema.ie>
2020-10-19 08:08:42 +0000

    Import output dataframe from geodir.py, CEA compatable to adjust for dwelling age

--- src/drem/transform/cso_sa.py
+++ src/drem/transform/cso_sa.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import geopandas as gpd
 from drem.filepaths import RAW_DIR
+from geodir.py import df_output
 
 # Spatially linking BER dataset to Geodir on SA level
 ber = pd.read_parquet(RAW_DIR / "BER_Closed.parquet")
@@ -25,17 +26,19 @@ census_renamed = census_extracted.rename(columns={"SMALL_AREA": "cso_small_area"
 ber_sa = pd.merge(ber_renamed, census_renamed, on="cso_small_area", how="inner")
 ber_sa.sort_values("cso_small_area")
 
-ber_test = ber_sa[ber_sa["Dublin Postcode"].str.contains("DUBLIN 14")]
-ber_test2 = ber_test[
-    ber_test["Dwelling Group"].str.contains("Semi detatched house", na=False)
-]
-ber_test2["Year of construction range"].value_counts()
+# ber_test = ber_sa[ber_sa["Dublin Postcode"].str.contains("DUBLIN 14")]
+# ber_test2 = ber_test[
+#    ber_test["Dwelling Group"].str.contains("Semi detatched house", na=False)
+# ]
+# ber_test2["Year of construction range"].value_counts()
 
 ber_crs = gpd.GeoDataFrame(ber_sa)
 ber_crs = ber_crs.to_crs(epsg=32629)
 
+# df_output from geodir.py
 geodir_in_ber = gpd.sjoin(ber_crs, df_output, op="contains")
 geo_dropped = geodir_in_ber.drop_duplicates(subset="Name", keep="last")
+# The construction period that's here is invalid and needs reassigning
 
 geo_dropped["Dwelling Group"] = geo_dropped["Dwelling type description"].map(
     {
@@ -54,9 +57,14 @@ geo_dropped["Dwelling Group"] = geo_dropped["Dwelling type description"].map(
 )
 
 # Assumption in medians here
+# Write a for loop here within "Apartment"
+# Need to assign these
+
+dwelling = ["Apartment", "Semi detatched house", "Detatched house", "Terraced"]
+
 geo_dwelling = geo_dropped[
-    geo_dropped["Dwelling Group"].str.contains("Semi detatched house", na=False)
+    geo_dropped["Dwelling Group"].str.contains("Apartment", na=False)
 ]
-geo_dropped.groupby(["cso_small_area"])[["Year of construction"]].median()
+geo_dwelling.groupby(["Dublin Postcode"])[["Year of construction"]].median()
 
 # Match median year of construction to pre-defined range
