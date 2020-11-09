aae9cd409d3477bb2a49000feb8faf98db8e1352
Oisin Doherty <oisin.doherty@codema.ie>
2020-11-09 14:42:03 +0000

    Manipulating the closed BER dataset in prefect flow context for synthetic residential stock

--- src/drem/modelling/res_build_stock.py
+++ src/drem/modelling/res_build_stock.py
@@ -10,5 +10,45 @@ energy demand in Dublin
 import pandas as pd
 import geopandas as gpd
 
+from pathlib import Path
+
+from prefect import Flow
+from prefect import Parameter
+from prefect import task
+
 from drem.filepaths import RAW_DIR
+
+@task 
+def _read_csv(input_filepath: Path) -> pd.DataFrame
+
+    return pd.read_csv(input_filepath, encoding="unicode_escape").drop_duplicates()
+
+
+@task
+def _assign_building_type(df: pd.DataFrame, on:str) -> pd.DataFrame
+
+    return df[on].map(
+        {
+            "Mid floor apt.": "Apartment",
+            "Top-floor apt.": "Apartment",
+            "Apt.": "Apartment",
+            "Maisonette": "Apartment",
+            "Grnd floor apt.": "Apartment",
+            "Semi-det. house": "Semi detatched house",
+            "House": "Semi detatched house",
+            "Det. house": "Detatched house",
+            "Mid terrc house": "Terraced house",
+            "End terrc house": "Terraced house",
+            "None": "Not stated",
+        }
+    )
+
+
+@task
+def _group_by_cso_small_area()
+
+
+
+with Flow as flow:
+
 
